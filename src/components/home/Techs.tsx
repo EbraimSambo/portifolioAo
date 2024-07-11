567aace38b67175da9439c1d316d8390b80cad5d
ebraimsambo <ebraimsambo@email.com>
2024-07-10 21:46:57 -0900

    byl

--- src/components/home/Techs.tsx
+++ src/components/home/Techs.tsx
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import React from 'react'
 import style from './style.module.sass'
 import { TextTech } from '@/utils/data/texts'
+import Link from 'next/link'
 const Techs = () => {
     return (
         <section className={style.techs}>
             <div>
                 <h2>Algumas Das Ferramentas utilizadas</h2>
                 <p> {TextTech} </p>
+                <Link href={"/techs"} >Ver outras tecnologias</Link>
             </div>
             <ul className={style.icons}>
                 {Images.map((tech, index) => (
